fa682f4e646e38806820bce1c7b8c79d2967a4ed
rafael01gx <rafael_junio_moraes@hotmail.com>
2025-11-27 20:21:33 -0300

    Definindo permissões

--- src/main/java/med/voll/web_application/domain/usuario/Usuario.java
+++ src/main/java/med/voll/web_application/domain/usuario/Usuario.java
@@ -1,14 +1,12 @@
 package med.voll.web_application.domain.usuario;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 
 @Entity
 @Table(name="usuarios")
@@ -20,18 +18,21 @@ public class Usuario implements UserDetails {
     private String nome;
     private String email;
     private String senha;
+    @Enumerated(EnumType.STRING)
+    private Perfil perfil;
 
     public Usuario() {}
 
-    public Usuario(String nome, String email, String senha) {
+    public Usuario(String nome, String email, String senha, Perfil perfil) {
         this.nome = nome;
         this.email = email;
         this.senha = senha;
+        this.perfil = perfil;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_" + perfil.name()));
     }
 
     @Override
@@ -51,4 +52,8 @@ public class Usuario implements UserDetails {
     public Long getId() {
         return id;
     }
+    public Perfil getPerfil() {
+        return perfil;
+    }
+
 }
